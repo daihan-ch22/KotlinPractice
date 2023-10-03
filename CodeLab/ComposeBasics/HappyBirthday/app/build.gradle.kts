4cedf51093b03d0fcfdaf27e4c53bf25e8a2d816
danc <daehan.dan.choi@gmail.com>
2023-10-03 20:22:35 +0900

    compose - HappyBirtyday(fin)

--- CodeLab/ComposeBasics/HappyBirthday/app/build.gradle.kts
+++ CodeLab/ComposeBasics/HappyBirthday/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.danc.happybirthday"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.danc.happybirthday"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
--- CodeLab/ComposeBasics/HappyBirthday/app/src/main/java/com/danc/happybirthday/MainActivity.kt
+++ CodeLab/ComposeBasics/HappyBirthday/app/src/main/java/com/danc/happybirthday/MainActivity.kt
@@ -3,13 +3,24 @@ package com.danc.happybirthday
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.danc.happybirthday.ui.theme.HappyBirthdayTheme
 
@@ -19,11 +30,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             HappyBirthdayTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    //..
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    GreetingImage(
+                        message = getString(R.string.happy_birthday_message),
+                        from = getString(R.string.from_who)
+                    )
                 }
             }
         }
@@ -32,19 +43,55 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GreetingText(message: String, from: String, modifier: Modifier = Modifier){
-    Text(
-        text = message,
-        fontSize = 100.sp,
-        lineHeight = 116.sp
-    )
-    Text(text = from, fontSize = 36.sp)
+    Column (
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+    ) {
+        Text(
+            text = message,
+            fontSize = 100.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = from,
+            fontSize = 36.sp,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.CenterHorizontally)
+        )
+    }
+}
+
+@Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier){
+    val image = painterResource(id = R.drawable.androidparty) //painterResource -> how to get image from R
+    Box {
+        Image (
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.5F
+        )
+        GreetingText(
+            message = message,
+            from = from,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp)
+        )
+    }
 }
 
 
-@Preview(showBackground = true)
+@Preview(showBackground = false)
 @Composable
 fun GreetingPreview() {
     HappyBirthdayTheme {
-        GreetingText(message = "Happy Blessed Birthday Dusty!", from = "from Dan")
+        //GreetingText(message = "Happy Birthday Dusty!", from = "from Dan")
+        GreetingImage(
+            message = stringResource(id = R.string.happy_birthday_message),
+            from = stringResource(id = R.string.from_who)
+        )
     }
 }
