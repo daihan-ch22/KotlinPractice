5c868ed18e5a0241bc1c0163868ddfc04d8b55f7
daehanchoi <daehanchoi@tsis.co.kr>
2023-10-24 13:05:00 +0900

    lemonade on going

--- CodeLab/ComposeBasics/Lemonade/app/src/main/java/com/example/lemonade/MainActivity.kt
+++ CodeLab/ComposeBasics/Lemonade/app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -1,18 +1,42 @@
 package com.example.lemonade
 
 import android.os.Bundle
+import android.view.RoundedCorner
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.lemonade.ui.theme.LemonadeTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,7 +49,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    LemonadeApp()
                 }
             }
         }
@@ -45,14 +69,38 @@ fun LemonadeApp(){
 
 
 @Composable
-fun LemonadeContents(){
+fun LemonadeContents(
+    modifier: Modifier = Modifier,
+    image: Painter,
+    description: String
+    ){
 
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-    ) {
-        Image(painter = painterResource(id = R.drawable.lemon_tree), contentDescription = "LemonTree")
+    var stringInfoHolder by remember {
+        mutableStateOf("")
     }
 
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ){
+        Box(
+            modifier = modifier.clip(RoundedCornerShape(10.dp))
+        ) {
+            Image(painter = painterResource(id = R.drawable.lemon_tree), contentDescription = "LemonTree",
+                modifier
+                    .background(
+                        Color.Cyan
+                    )
+                    .size(200.dp)
+                    .padding(10.dp)
+                    .clickable {
+                        //TODO onclick event
+                    })
+        }
+
+        Spacer(modifier = modifier.size(16.dp))
+
+        Text(text = stringResource(id = R.string.TAP_LEMON), fontWeight = FontWeight.Bold, fontSize = 18.sp)
+    }
 }
 
