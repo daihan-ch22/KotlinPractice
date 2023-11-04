e02142a4849609459c5dec3fb43c1fd1837f4375
danc <daehan.dan.choi@gmail.com>
2023-11-04 16:43:58 +0900

    state intro & state hoisting

--- CodeLab/ComposeBasics/basic-android-kotlin-compose-training-tip-calculator-starter/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ CodeLab/ComposeBasics/basic-android-kotlin-compose-training-tip-calculator-starter/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -28,14 +28,21 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.tiptime.ui.theme.TipTimeTheme
@@ -59,6 +66,17 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun TipTimeLayout() {
+
+    //state hoisting
+    var amountInput by remember {
+        mutableStateOf("")
+    }
+    //toDoubleOrNull == parse String value to Double. if it can't be converted, return null
+    //in this case, if value is null it will be 0.0
+    val amount = amountInput.toDoubleOrNull() ?: 0.0
+
+    val tip = calculateTip(amount)
+
     Column(
         modifier = Modifier
             .statusBarsPadding()
@@ -74,10 +92,15 @@ fun TipTimeLayout() {
                 .align(alignment = Alignment.Start)
         )
 
-        EditNumberField(modifier = Modifier.padding(bottom = 32.dp).fillMaxWidth())
+        EditNumberField(
+            value = amountInput,
+            onValueChange = { amountInput = it },
+            modifier = Modifier
+            .padding(bottom = 32.dp)
+            .fillMaxWidth())
 
         Text(
-            text = stringResource(R.string.tip_amount, "$0.00"),
+            text = stringResource(R.string.tip_amount, tip),
             style = MaterialTheme.typography.displaySmall
         )
         Spacer(modifier = Modifier.height(150.dp))
@@ -103,10 +126,26 @@ fun TipTimeLayoutPreview() {
 }
 
 @Composable
-fun EditNumberField(modifier: Modifier = Modifier){
+fun EditNumberField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier
+){
     TextField(
-        value = "",
-        onValueChange = {},
+        value = value,
+        onValueChange = onValueChange,
+
+        //hint
+        label = {
+            Text(text = stringResource(id = R.string.bill_amount))
+        },
+
+        //horizontal scroll
+        singleLine = true,
+
+        //Popup keyboard setting
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+
         modifier = modifier
     )
 }
