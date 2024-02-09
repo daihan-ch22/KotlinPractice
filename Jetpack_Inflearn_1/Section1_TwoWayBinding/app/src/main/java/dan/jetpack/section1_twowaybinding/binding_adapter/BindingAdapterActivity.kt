4ebee2f3a21511c448819d5c266fb18cbb16fe22
danc <daehan.dan.choi@gmail.com>
2024-02-09 20:42:50 +0900

    BindingAdapter1 (no use)

--- Jetpack_Inflearn_1/Section1_TwoWayBinding/app/src/main/java/dan/jetpack/section1_twowaybinding/binding_adapter/BindingAdapterActivity.kt
+++ Jetpack_Inflearn_1/Section1_TwoWayBinding/app/src/main/java/dan/jetpack/section1_twowaybinding/binding_adapter/BindingAdapterActivity.kt
@@ -2,11 +2,35 @@ package dan.jetpack.section1_twowaybinding.binding_adapter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import dan.jetpack.section1_twowaybinding.R
 
+/**
+ * BindingAdapter -> View에 속성을 맘대로 수정
+ *
+ * 1. 기존에 양방향 데이터 결합 / BindingAdapter 미사용
+ * 2. 양방향 데이터 결합 / BindingAdapter 사용
+ */
 class BindingAdapterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_binding_adapter)
+
+        var age: Int = 0
+        val ageArea = findViewById<TextView>(R.id.ageArea_bindingAdapter)
+        val imageArea = findViewById<ImageView>(R.id.imgArea_bindingAdapter)
+        val plus = findViewById<Button>(R.id.plus_bindingAdapter)
+
+        plus.setOnClickListener {
+            age++
+
+            ageArea.text = age.toString()
+
+            if(age > 20){
+                imageArea.setImageResource(R.drawable.download)
+            }
+        }
     }
 }
