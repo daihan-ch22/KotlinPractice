97ba8d3602fb6c209c464380728c2d390b9bbc95
danc <daehan.dan.choi@gmail.com>
2024-02-17 18:41:19 +0900

    Section3 - XML init

---
+++ Jetpack_Inflearn_2/Section3_RoomAdvanced_CRUD/settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Section3_RoomAdvanced_CRUD"
+include(":app")
