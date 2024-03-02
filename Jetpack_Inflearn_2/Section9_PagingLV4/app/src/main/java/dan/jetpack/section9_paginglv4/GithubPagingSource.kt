8a32e4bca0a7075230b0adec93016b449c9ac3ce
danc <daehan.dan.choi@gmail.com>
2024-03-02 18:25:48 +0900

    Section9 - test error

--- Jetpack_Inflearn_2/Section9_PagingLV4/app/src/main/java/dan/jetpack/section9_paginglv4/GithubPagingSource.kt
+++ Jetpack_Inflearn_2/Section9_PagingLV4/app/src/main/java/dan/jetpack/section9_paginglv4/GithubPagingSource.kt
@@ -1,34 +1,64 @@
 package dan.jetpack.section9_paginglv4
 
+import android.util.Log
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
 import dan.jetpack.section9_paginglv4.data.Items
 import dan.jetpack.section9_paginglv4.network.GitApi
 import kotlinx.coroutines.delay
+import java.lang.Exception
 
 
 private const val STARTING_KEY = 1
 
-class GithubPagingSource (
+class GithubPagingSource(
     private val gitApi: GitApi,
-    private val query : String
+    private val query: String
 ) : PagingSource<Int, Items>() {
 
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Items> {
 
         delay(1000)
 
-        val page = params.key ?: STARTING_KEY
+        return try {
+            val page = params.key ?: STARTING_KEY
+            val response = gitApi.getData(query, page, params.loadSize)
+            val data = response.items
 
-        val response = gitApi.getData(query, page, params.loadSize)
+            // force error start
+            var count = 0
 
-        val data = response.items
+            if(page != 1) {
+                count = (0..1).random()
+                Log.d("count", count.toString())
+            }
 
-        return LoadResult.Page(
-            data = data,
-            prevKey = if (page == 1) null else page -1,
-            nextKey = page + (params.loadSize / 30)
-        )
+            if(count == 1) {
+                throw Exception("exception!")
+            }
+            // force error end
+
+            LoadResult.Page(
+                data = data,
+                prevKey = if (page == 1) null else page - 1,
+                nextKey = page + (params.loadSize / 30)
+            )
+
+        } catch (e: Exception) {
+            LoadResult.Error(e)
+        }
+
+//        val page = params.key ?: STARTING_KEY
+//
+//        val response = gitApi.getData(query, page, params.loadSize)
+//
+//        val data = response.items
+//
+//        return LoadResult.Page(
+//            data = data,
+//            prevKey = if (page == 1) null else page -1,
+//            nextKey = page + (params.loadSize / 30)
+//        )
     }
 
     override fun getRefreshKey(state: PagingState<Int, Items>): Int? {
